2e8e20a2c95fe440326171edc96e9f9794027a6d
Xavier Mathew <xavier@compile.com>
2020-09-06 02:24:57 +0530

    (fix) bad merge

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -367,7 +367,6 @@ default_query_runners = [
     "redash.query_runner.exasol",
     "redash.query_runner.cloudwatch",
     "redash.query_runner.cloudwatch_insights",
->>>>>>> master
 ]
 
 enabled_query_runners = array_from_string(
